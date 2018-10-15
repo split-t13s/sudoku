6d3badad95da3aac569f0fac871a8974939968a5
MS <email@example.com>
2018-10-15 11:33:43 +0100

    Cover/Uncover and comments
    Cover() method removes node butkeeps links.
    Uncover() method returns removed node.
    Comments added to methods.

--- dlx/Node.java
+++ dlx/Node.java
@@ -12,20 +12,46 @@ public class Node {
         prev = null;
     }
 
-    public Node insert(Node root, int label) {
+    /**
+     * Inserts node at the end of the list.
+     * @param root - the first node of the list
+     * @param label
+     */
+    public void insert(Node root, int label) {
         Node newnode = new Node(label);
         newnode.setNext(root);
         newnode.setPrev(root.getPrev());
         newnode.getNext().setPrev(newnode);
         newnode.getPrev().setNext(newnode);
-        return newnode;
     }
 
+    /**
+     * Remove node from list and update links
+     * @param node - the removed node
+     */
     public void remove(Node node) {
         node.getNext().setPrev(node.getPrev());
         node.getPrev().setNext(node.getNext());
         node.setNext(null);
         node.setPrev(null);
+    }
+
+    /**
+     * Remove node from list but keep links.
+     * @param node - the removed node
+     */
+    public void cover(Node node) {
+        node.getNext().setPrev(node.getPrev());
+        node.getPrev().setNext(node.getNext());
+    }
+
+    /**
+     * Return a removed node to the list.
+     * @param node - the returned node
+     */
+    public void uncover(Node node) {
+        node.getNext().setPrev(node);
+        node.getPrev().setNext(node);
     }
 
     public Node getPrev() {
